8f7aed0f7a740e11f26b27a49020135f5a9538a8
Allilouy <47003777+Allilouy@users.noreply.github.com>
2019-07-08 20:50:00 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Image from './imageInSrc.jpg'
+import MediaCard from './card'
+
+
+function App() {
+  return (
+    <div className="App">
+      <div class="container">
+  <div class="card">
+  <MediaCard />
+  </div>
+</div>
+      
+      </div>
+  );
+}
+
+export default App;
